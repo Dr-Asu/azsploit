6056548edaff85a31946fc1a0b947c9f4eefa639
Dr-Asu <99238462+Dr-Asu@users.noreply.github.com>
2023-07-26 09:19:31 +0500

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,51 @@
+chmod +x azsploit
+cp -r azsploit /usr/local/sbin
+echo "rm -rf /usr/local/sbin/azsploit && echo -e '\e[1;31m    
+
+                       (UNINSTALLATION COMPLETED)
+
+\e[0m' && rm uninstaller.sh " > uninstaller.sh 
+
+
+
+echo -e '\e[1;33m
+    ___                       __        _  __ 
+   /   | ____   _____ ____   / /____   (_)/ /_
+  / /| |/_  /  / ___// __ \ / // __ \ / // __/
+ / ___ | / /_ (__  )/ /_/ // // /_/ // // /_  
+/_/  |_|/___//____// .___//_/ \____//_/ \__/  
+                  /_/                          (Linux)
+                 /_/                        \e[1;34m
+                                
+                                Created by "MUHAMMAD ASIF Aka ASU"
+                                ACE CONNECT MULTAN\e[1;31m
+
+
+  Usage of AZSPLOIT for attacking targets without prior mutual consent is
+  ILLEGAL. Developers are not responsible for any damage caused by this script.
+  AZSPLOIT is intented ONLY FOR EDUCATIONAL PURPOSES!!! STAY LEGAL!!!
+
+\e[1;36m
+                      
+                       (INSTALLATION COMPLETED)
+
+                                                             \e[1;32m
+
+  To execute AZSPLOIT type anywhere in your terminal "azsploit".  \e[0m 
+'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
---
+++ uninstaller.sh
@@ -0,0 +1,5 @@
+rm -rf /usr/local/sbin/azsploit && echo -e '[1;31m    
+
+                       (UNINSTALLATION COMPLETED)
+
+[0m' && rm uninstaller.sh 
